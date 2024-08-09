d4ab6a8c336c421103bdfb018f6167c8960afd3d
DeepOP <bdeeps453@gmail.com>
2024-08-09 21:17:33 +0530

    changes

--- trail.cpp
+++ trail.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class InstructionFetch {
 public:
     uint32_t fetchInstruction(uint32_t pc, const vector<uint8_t>& memory) {
-        // Assuming instruction size is 4 bytes
+        
         uint32_t instruction = memory[pc] << 24 | memory[pc + 1] << 16 | memory[pc + 2] << 8 | memory[pc + 3];
         return instruction;
     }
@@ -21,7 +21,6 @@ public:
     };
 
     DecodedInstruction decode(uint32_t instruction) {
-        // Simple decoding: assuming the opcode is in the first 8 bits
         uint8_t opcode = instruction >> 24;
         uint32_t address = instruction & 0x00FFFFFF;
 
@@ -38,9 +37,7 @@ public:
 class Execute {
 public:
     void execute(const std::string& operation, uint32_t& address) {
-        // In this simple example, we only have LOAD, so no actual operation is performed
         if (operation == "LOAD") {
-            // Address remains the same
         }
     }
 };
